7796e063a10fb82ddf9a415228fd8dfc88c2dbc2
Hannes Tydén <hannes@soundcloud.com>
2012-08-10 00:02:22 +0200

    Add full status code support.
    RFCs 2324, 2616, 4918 and 6585.

--- lib/hyperspec.rb
+++ lib/hyperspec.rb
@@ -136,16 +136,73 @@ module HyperSpec
 
   Response = Struct.new(:status_code, :headers, :body) do
     STATI = {
-      # 2xx
+      # RFC 2616 - HTTP 1.1
+      ## Informational
+      100 => :continue,
+      101 => :switching_protocols,
+
+      ## Successful 2xx
       200 => :ok,
       201 => :created,
+      202 => :accepted,
+      203 => :non_authoritative_information,
+      204 => :no_content,
+      205 => :reset_content,
+      206 => :partial_content,
 
-      # 4xx
+      ## Redirection 3xx
+      300 => :multiple_choices,
+      301 => :moved_permanently,
+      302 => :found,
+      303 => :see_other,
+      304 => :not_modified,
+      305 => :use_proxy,
+    # 306 => :(Unused),
+      307 => :temporary_redirect,
+
+      ## Client Error 4xx,
+      400 => :bad_request,
       401 => :unauthorized,
+      402 => :payment_required,
+      403 => :forbidden,
+      404 => :not_found,
+      405 => :method_not_allowed,
+      406 => :not_acceptable,
+      407 => :proxy_authentication_required,
+      408 => :request_timeout,
+      409 => :conflict,
+      410 => :gone,
       411 => :length_required,
+      412 => :precondition_failed,
+      413 => :request_entity_too_large,
+      414 => :request_uri_too_long,
+      415 => :unsupported_media_type,
+      416 => :requested_range_not_satisfiable,
+      417 => :expectation_failed,
 
-      # WebDav extensions
+      ## Server Error 5xx
+      500 => :internal_server_error,
+      501 => :not_implemented,
+      502 => :bad_gateway,
+      503 => :service_unavailable,
+      504 => :gateway_timeout,
+      505 => :http_version_not_supported,
+
+      # RFC 2324
+      418 => :im_a_teapot,
+
+      # RFC 4918 - WebDav
+      207 => :multi_status,
       422 => :unprocessable_entity,
+      423 => :locked,
+      424 => :failed_dependency,
+      507 => :insufficient_storage,
+
+      # RFC 6585 - Additional HTTP Status Codes
+      428 => :precondition_required,
+      429 => :too_many_requests,
+      431 => :request_header_fields_too_large,
+      511 => :network_authentication_required,
     }
 
     def self.from_net_http_response(http)
