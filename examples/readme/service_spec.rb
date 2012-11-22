26b23e88ad41f55fee1d61c92ef7e1c253f26763
Hannes Tydén <hannes@tyden.name>
2012-11-22 18:25:51 +0100

    Style changes of example spec.

--- examples/readme/service_spec.rb
+++ examples/readme/service_spec.rb
@@ -11,44 +11,58 @@ require 'json'
 # Run specs: `be ruby -rubygems examples/readme/service_spec.rb`.
 
 service "http://localhost:4567" do
-  def response_json
+  def responds_with_json_where
     JSON.parse(response.body)
   end
 
   resource "/lolz" do
     get do
+
       it { responds_with.status :ok }
-      it { response_json['lolz'].must_be_instance_of Array }
+
+      it { responds_with_json_where['lolz'].must_be_instance_of Array }
 
       with_query("q=monorail") do
+
         it "only lists lolz that match the query" do
-          response_json['lolz'].wont_be_empty
-          response_json['lolz'].each do |lol|
+          responds_with_json_where['lolz'].wont_be_empty
+          responds_with_json_where['lolz'].each do |lol|
             lol['title'].must_match /monorail/
           end
         end
+
       end
 
       with_query("q=looong") do
+
         it "only lists lolz that match the query" do
-          response_json['lolz'].wont_be_empty
-          response_json['lolz'].each do |lol|
+          responds_with_json_where['lolz'].wont_be_empty
+          responds_with_json_where['lolz'].each do |lol|
             lol['title'].must_match /looong/
           end
         end
+
       end
     end
 
     post do
       describe "without request body" do
+
         it { responds_with.status :unprocessable_entity }
+
       end
 
       describe "with request body" do
         with_headers({ 'Content-Type' => 'application/json' }) do
           with_request_body({ "title" => "Roflcopter!" }.to_json) do
+
             it { responds_with.status :created }
-            it { response_json['lol']['title'].must_equal 'Roflcopter!' }
+
+            it do
+              responds_with_json_where['lol']['title'].
+                must_equal 'Roflcopter!'
+            end
+
           end
         end
       end
